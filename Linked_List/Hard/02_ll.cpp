be0ca646d2f0cf7ea3b7dc14019339129aa285a6
kinjaldesarla <kinjaljain107@gmail.com>
2026-02-04 22:23:07 +0530

    linkedlist problems

--- Linked_List/Hard/02_ll.cpp
+++ Linked_List/Hard/02_ll.cpp
@@ -4,7 +4,6 @@ class Node{
     public:
     int data;
     Node*next;
-
     public:
     Node(int data1){
         data=data1;
@@ -30,6 +29,7 @@ Node* array_linkedlist(vector<int>arr){
      }
      return head;
 }
+
 // 1. merge k sorted list
 // brute force-  TC==>o(n*k) + o(mlogm) + o(m)    SC==>o(2n*k)
 // store to array-> sort them -> convert array to linkedlist
@@ -84,6 +84,99 @@ Node* merge_klist1(vector<Node*>list){
     }
     return dummynode->next;
 }
+
+// 2. Clone a Linked List with random and next pointer
+// brute force-  TC==>o(2n)  SC==>o(2n)
+Node* clone(Node* head){
+    Node* temp=head;
+    map<Node*,Node*>mpp;
+    while(temp!=NULL){
+        Node* newnode=new Node(temp->data);
+        mpp[temp]=newnode;
+        temp=temp->next;
+    }
+    temp=head;
+    while(temp!=NULL){
+        Node* copynode=mpp[temp];
+        copynode->next=mpp[temp->next];
+        copynode->random=mpp[temp->random]; 
+        temp=temp->next;
+    }
+    return mpp[head];
+}
+//optimal-  TC==>o(3n)   SC==>o(n)
+Node* clone1(Node* head){
+    Node* temp=head;
+    while(temp!=NULL){
+        Node* copynode=new Node(temp->data);
+        copynode->next=temp->next;
+        temp->next=copynode;
+        temp=temp->next->next;
+    }
+    temp=head;
+    while(temp!=NULL){
+        Node* copynode=temp->next;
+        if(temp->random)copynode->random=temp->random->next;
+        else copynode->random=NULL;
+        temp=temp->next->next;
+    }
+    temp=head;Node* dummynode=new Node(-1);
+    Node* current=dummynode;
+    while(temp!=NULL){
+        current->next=temp->next;
+        temp->next=temp->next->next;
+        current=current->next;
+        temp=temp->next;
+    }
+    return dummynode->next;
+}
+
+// 3. design browser history
+class BNode{
+    public:
+    string data;
+    BNode* front;
+    BNode* back;
+    public:
+    BNode(string data1){
+        data=data1;
+        front=nullptr;
+        back=nullptr;
+    }
+};
+class browser{
+    BNode* current;
+    public:
+    //o(1)
+    browser(string &url){
+        current=new BNode(url);
+    }
+    //o(1)
+    void visit(string url){
+        BNode* newnode= new BNode(url);
+        current->front=newnode;
+        newnode->back=current;
+        current=newnode;
+    }
+    //o(steps)
+    string backward(int steps){
+        while(steps){
+            if(current->back)current=current->back;
+            else break;
+            steps--;
+        }
+        return current->data;
+    }
+    //o(steps)
+    string forward(int steps){
+        while(steps){
+            if(current->front)current=current->front;
+            else break;
+            steps--;
+        }
+        return current->data;
+    }
+};
 
 int main(){
     return 0;
