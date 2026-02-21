8c56b6529ba20a6fb76345b6fba8040209a817ab
kinjaldesarla <kinjaljain107@gmail.com>
2026-02-21 19:46:53 +0530

    N Queen and Rat in a Maze problem

--- Recursion/Hard/01_recursion.cpp
+++ Recursion/Hard/01_recursion.cpp
@@ -65,13 +65,102 @@ bool word_search(vector<vector<char>> &board,string word){
     }
 }
 
+// 3. N Queen
+void nQueen_recu(int col,int n, vector<vector<string>> &ans,vector<string> &board,vector<int> &leftRow,vector<int> &upperDaigonal,vector<int> &lowerDaigonal){
+    if(col==n){
+        ans.push_back(board);
+        return;
+    }
+    for(int i=0;i<n;i++){
+        if(leftRow[i]==0 && upperDaigonal[(n-1)+(col-i)]==0 && lowerDaigonal[(i+col)]==0){
+            board[i][col]='Q';
+            leftRow[i]=1;
+            upperDaigonal[(n-1)+(col-i)]=1;
+            lowerDaigonal[i+col]=1;
+            nQueen_recu(col+1,n,ans,board,leftRow,upperDaigonal,lowerDaigonal);
+            board[i][col]='.';
+             leftRow[i]=0;
+            upperDaigonal[(n-1)+(col-i)]=0;
+            lowerDaigonal[i+col]=0;
+        }
+    }
+}
+vector<vector<string>> nQueen(int n){
+     vector<vector<string>> ans;
+    vector<string> board;
+    string s=".";
+    for(int i=0;i<4;i++){
+      board.push_back(s);
+    }
+    // hashing
+    vector<int> leftRow(n,0), upperDaigonal(2*n-1,0), lowerDaigonal(2*n-1,0);
+    nQueen_recu(0,n,ans,board,leftRow,upperDaigonal,lowerDaigonal);
+    return ans;
+}
+
+// 4. Rat in a Maze  TC==>o(4^(n*m))  SC==>o(n*m)
+void ratRecu(int i,int j,int n,vector<vector<int>> &maze,string path,vector<string> &ans,vector<vector<int>> &vis){
+    if(i==n-1 && j==n-1){
+        ans.push_back(path);
+        return;
+    }
+// instead of writing four if statement 
+    // // downward
+    // if(i+1<n && !vis[i+1][j] && maze[i+1][j]==1){
+    //     vis[i][j]=1;
+    //     ratRecu(i+1,j,n,maze,path+"D",ans,vis);
+    //     vis[i][j]=0;
+    // }
+    // // left
+    // if(j-1>=0 && !vis[i][j-1] && maze[i][j-1]==1){
+    //     vis[i][j]=1;
+    //     ratRecu(i,j-1,n,maze,path+"L",ans,vis);
+    //     vis[i][j]=0;
+    // }
+    // // right
+    // if(j+1<n && !vis[i][j+1] && maze[i][j+1]==1){
+    //     vis[i][j]=1;
+    //     ratRecu(i,j+1,n,maze,path+"R",ans,vis);
+    //     vis[i][j]=0;
+    // }
+    // // upward
+    // if(i-1>=0 && !vis[i-1][j] && maze[i-1][j]==1){
+    //     vis[i][j]=1;
+    //     ratRecu(i-1,j,n,maze,path+"U",ans,vis);
+    //     vis[i][j]=0;
+    // }
+
+// better one is 
+    string dir="DLRU";
+    int di[]={+1,0,0,-1};
+    int dj[]={0,-1,+1,0};
+    for(int ind=0;ind<4;ind++){
+        int nexti=i+di[ind];
+        int nextj=j+dj[ind];
+        if(nexti<n && nextj<n && nexti>=0 && nextj>=0 && !vis[nexti][nextj] && maze[nexti][nextj]==1){
+            vis[i][j]=1;
+            ratRecu(nexti,nextj,n,maze,path+dir[ind],ans,vis);
+            vis[i][j]=0;
+        }
+    }
+
+}
+void rat_in_maze(vector<vector<int>>maze,int n){
+    vector<string>ans;
+    vector<vector<int>> vis(n,vector<int>(n,0));
+   if(maze[0][0]==1) ratRecu(0,0,n,maze,"",ans,vis);
+   for(auto it:ans)cout<<it<<" ";
+}
+
 int main(){
     // palind("aabb");
-    vector<vector<char>> board = {
-        {'A','B','C','E'},
-        {'S','F','C','S'},
-        {'A','D','E','E'}
-    };
-   cout<< word_search(board,"ABCB");
+//     vector<vector<char>> board = {
+//         {'A','B','C','E'},
+//         {'S','F','C','S'},
+//         {'A','D','E','E'}
+//     };
+//    cout<< word_search(board,"ABCB");
+      vector<vector<int>> maze = {{1, 0, 0, 0},{1, 1, 0, 1},{1, 1, 0, 0},{0, 1, 1, 1}};
+      rat_in_maze(maze,4);
     return 0;
 }
